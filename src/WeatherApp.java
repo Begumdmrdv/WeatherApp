3e5d1cba0b9e7f33013d791b40b8f1181a06947a
Begumdmrdv <begumdmrdv35@gmail.com>
2024-09-05 17:24:25 +0300

    Update WeatherApp.java

--- src/WeatherApp.java
+++ src/WeatherApp.java
@@ -56,6 +56,15 @@ public class WeatherApp extends JFrame {
         weatherConditionImage.setBounds(0,125,450,217);
         add(weatherConditionImage);
 
+        // temperature text
+        JLabel temperatureText = new JLabel("10 C");
+        temperatureText.setBounds(0,350, 450, 54);
+        temperatureText.setFont((new Font("Dialog", Font.BOLD, 48)));
+
+        // center the text
+        temperatureText.setHorizontalAlignment(SwingConstants.CENTER);
+        add(temperatureText);
+
     }
 
     // used to create images in our gui components
